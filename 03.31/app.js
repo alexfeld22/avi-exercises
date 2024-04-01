5f145e76d17f1aae3e8703dcb9a2032d5dda8d3c
alex_feldshteyn <alexey.israel22@gmail.com>
2024-04-01 11:55:07 +0300

    corrections after feedback

--- 03.31/app.js
+++ 03.31/app.js
@@ -5,12 +5,13 @@ btn.addEventListener('click',changeBackgroundColor);
 
 function changeBackgroundColor(){
     const userColor = document.getElementsByTagName('input')[0].value;
-    // console.log (userColor);
     if(isValidHexColor(userColor)){
         document.body.style.backgroundColor = userColor;
-    }else{
-        window.alert(`${userColor} is not a correct HEX color`);
+        return;
     }
+    
+    window.alert(`${userColor} is not a correct HEX color`);
+    return;
 }
 
  function isValidHexColor(color) {
